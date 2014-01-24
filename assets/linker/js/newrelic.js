dfe6ab97ac9ba7c33b437661eefaa85ae815096e
b87775 <jwoods1@fullsail.edu>
2014-01-23 22:19:52 -0700

    added new relic again again

---
+++ assets/linker/js/newrelic.js
@@ -0,0 +1,24 @@
+/**
+ * New Relic agent configuration.
+ *
+ * See lib/config.defaults.js in the agent distribution for a more complete
+ * description of configuration variables and their potential values.
+ */
+exports.config = {
+  /**
+   * Array of application names.
+   */
+  app_name : ['dhWedding'],
+  /**
+   * Your New Relic license key.
+   */
+  license_key : '71dd9a6834d750d0b740a0d85a10fd2c549e880b',
+  logging : {
+    /**
+     * Level at which to log. 'trace' is most useful to New Relic when diagnosing
+     * issues with the agent, 'info' and higher will impose the least overhead on
+     * production applications.
+     */
+    level : 'trace'
+  }
+};
